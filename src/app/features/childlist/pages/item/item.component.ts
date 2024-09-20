48d6972ed7dd8f13d8e19c5ec2841f8f908a92d7
Steven Hanse (Malashuy) <steven.hanse@live.be>
2024-09-20 16:47:32 +0200

    Avance sur la prod

--- src/app/features/childlist/pages/item/item.component.ts
+++ src/app/features/childlist/pages/item/item.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from "@angular/router";
 import {ChildlistitemDtoModel} from "../../models/childlistitem.dto.model";
 import {faCartPlus} from "@fortawesome/free-solid-svg-icons";
 import {ChildlistitemService} from "../../services/childlistitem.service";
+import {ToastService} from "../../../../shared/services/toast.service"
 
 @Component({
   selector: 'app-item',
@@ -15,7 +16,8 @@ export class ItemComponent {
 
     constructor(
         private readonly _ar: ActivatedRoute,
-        private readonly _cli: ChildlistitemService
+        private readonly _cli: ChildlistitemService,
+        private readonly _toast: ToastService
     )
     {
         this._ar.data.subscribe((data: any) => {
@@ -26,6 +28,7 @@ export class ItemComponent {
 
     addToCart(id:string) {
         this._cli.addToCart(id)
+        this._toast.show({template: standardTpl , classname: 'bg-success text-light', delay: 10000 })
     }
 
     protected readonly faCartPlus = faCartPlus;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,23 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { FormErrorComponent } from './components/form-error/form-error.component';
+import { ToastGlobalComponent } from './components/toast-global/toast-global.component';
+import {NgbToast, NgbTooltipModule} from "@ng-bootstrap/ng-bootstrap";
+import { ToastContainerComponent } from './components/toast-container/toast-container.component'
 
 
 @NgModule({
     declarations: [
-    FormErrorComponent
+    FormErrorComponent,
+    ToastGlobalComponent,
+    ToastContainerComponent
   ],
     imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
+        NgbTooltipModule,
+        NgbToast,
     ],
     exports: [
         FormsModule,
